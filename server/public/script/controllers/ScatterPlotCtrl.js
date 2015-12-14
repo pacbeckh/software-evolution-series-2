8999c1c3be2d53350c15a9bce365fedb291c74ef
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-12-14 14:08:22 +0100

    Added fixes for fuck up

--- server/public/script/controllers/ScatterPlotCtrl.js
+++ server/public/script/controllers/ScatterPlotCtrl.js
@@ -1,200 +1,198 @@
-angular.module('CloneDetection').controller('ScatterPlotCtrl', function($scope, $timeout) {
+angular.module('CloneDetection').controller('ScatterPlotCtrl', function ($scope, $timeout) {
 
-    var selectClassesByDataItem = function (uids, clones) {
-        $scope.activeClasses = clones.filter(function (cloneClass) {
-            return _.contains(uids, cloneClass.uid);
-        });
-        $scope.activeClasses.forEach(function (e) {
-            e.active = false;
-        });
-        $scope.activeClasses[0].active = true;
-    };
+  var selectClassesByDataItem = function (uids, clones) {
+    $scope.activeClasses = clones.filter(function (cloneClass) {
+      return _.contains(uids, cloneClass.uid);
+    });
+    $scope.activeClasses.forEach(function (e) {
+      e.active = false;
+    });
+    $scope.activeClasses[0].active = true;
+  };
 
-    var BUCKET_COUNT = 8;
+  var BUCKET_COUNT = 8;
 
-    $scope.chartConfig = {};
+  $scope.chartConfig = {};
 
-    function generateColors(n){
-    	result = [];
+  function generateColors(n) {
+    var result = [];
 
-    	colorRange = 100 / (n);
+    var colorRange = 100 / (n);
 
-    	for(i = 0; i < (n) ; i++ ){
-    		R = Math.floor((255 * (colorRange * i)) / 100);
-			G = Math.floor((255 * (100 - (colorRange * i))) / 100);
-			B = 0;
+    for (i = 0; i < (n); i++) {
+      var R = Math.floor((255 * (colorRange * i)) / 100);
+      var G = Math.floor((255 * (100 - (colorRange * i))) / 100);
+      var B = 0;
 
-			result.push("rgba("+R+","+G+","+B+",0.8)");
-    	}
-
-    	return result;
+      result.push("rgba(" + R + "," + G + "," + B + ",0.8)");
     }
 
+    return result;
+  }
 
 
-	var chartConfig = function () { return {
-		options: {
-			chart: {
-				type: 'scatter',
-				height: 550,
-				zoomType: 'xy'
-			},
-			legend: {
-				layout: 'horizontal',
-//				align: 'left',
-//				verticalAlign: 'top',
-//				x: 100,
-//				y: 70,
-//				floating: true,
-//				backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-//				borderWidth: 1
-			},
-			plotOptions: {
-				scatter: {
-                    marker: {
-                        radius: 5,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-					animation: false,
-					point: {
-						events: {
-							click: goToClones
-						}
-					},
-					states: {
-						hover: {
-							marker: {
-								enabled: false
-							}
-						}
-					},
-					tooltip: {
-						headerFormat: '<b>{series.name} clone classes</b><br>',
-						pointFormat: '{point.x} weight</br> {point.y} fragments'
-					}
-				}
-			}
-		},
-		title: {
-			text: 'Clone class distribution'
-		},
-		xAxis: {
-		  allowDecimals: false,
-			title: {
-				enabled: true,
-				text: 'Weight'
-			},
-			startOnTick: true,
-			endOnTick: false,
-			showLastLabel: true
-		},
-		yAxis: {
-			title: {
-				text: 'Number of fragments'
-			},
-			endOnTick: false,
-			showLastLabel: true
-		}
-	};};
-
-	function goToClones(d) {
-		$scope.$state.go("app.clones", {
-			weight: this.x,
-			fragments: this.y
-		});
-
-		return false;
-	}
-
-	function createSeries(clones) {
-		mapping = {};
-		clones.forEach(function(clazz) {
-			mapping[clazz.weight] = mapping[clazz.weight] || [];
-			mapping[clazz.weight][clazz.fragments.length] = mapping[clazz.weight][clazz.fragments.length] || [];
-			mapping[clazz.weight][clazz.fragments.length].push(clazz.uid);
-		});
-
-		var data = {};
-		Object.keys(mapping).forEach(function(w) {
-			Object.keys(mapping[w]).forEach(function(fs) {
-			    key = mapping[w][fs].length;
-			    point = [parseInt(w),parseInt(fs)];
-			    if(data[key] === undefined) {
-			        data[key] = {
-			            name : key,
-			            data : [point]
-			        };
-			    } else {
-			        data[key].data.push(point);
-			    }
-			});
-		});
-
-		sortedKeys = _.sortBy(_.map(_.keys(data), function(key){return parseInt(key);}), function(key){return key;});
-
-		size = sortedKeys.length;
-		bucketSize = Math.floor(size / BUCKET_COUNT);
-		extra = size % BUCKET_COUNT;
-		taken = 0;
-
-		//create separate series for the first bucket.
-
-		series = [];
-
-		_.take(sortedKeys, bucketSize).forEach(function (key){
-			series.push(data[key]);
-		});
-		taken += bucketSize
-
-		while(taken != size) {
-			take = bucketSize;
-			if(extra > 0 ){
-				take += 1;
-				extra -= 1;
-			}
-			keysToTake = sortedKeys.slice(taken, (taken + take));
-			serie = {
-				name : _.first(keysToTake) + " - " + _.last(keysToTake),
-				data : []
-			}
-
-			keysToTake.forEach(function (key){
-				serie.data = serie.data.concat(data[key].data);
-			});
-
-			taken += take;
-			series.push(serie);
-		}
-
-        w = $scope.$state.params.weight;
-        fs =  $scope.$state.params.fragments;
-        if (w && fs) {
-            selectClassesByDataItem(mapping[w][fs], clones);
+  var chartConfig = function () {
+    return {
+      options: {
+        chart: {
+          type: 'scatter',
+          height: 550,
+          zoomType: 'xy'
+        },
+        legend: {
+          layout: 'horizontal'
+        },
+        plotOptions: {
+          scatter: {
+            marker: {
+              radius: 5,
+              states: {
+                hover: {
+                  enabled: true,
+                  lineColor: 'rgb(100,100,100)'
+                }
+              }
+            },
+            animation: false,
+            point: {
+              events: {
+                click: goToClones
+              }
+            },
+            states: {
+              hover: {
+                marker: {
+                  enabled: false
+                }
+              }
+            },
+            tooltip: {
+              headerFormat: '<b>{series.name} clone classes</b><br>',
+              pointFormat: '{point.x} weight</br> {point.y} fragments'
+            }
+          }
         }
+      },
+      title: {
+        text: 'Clone class distribution'
+      },
+      xAxis: {
+        allowDecimals: false,
+        title: {
+          enabled: true,
+          text: 'Weight'
+        },
+        startOnTick: true,
+        endOnTick: false,
+        showLastLabel: true
+      },
+      yAxis: {
+        title: {
+          text: 'Number of fragments'
+        },
+        endOnTick: false,
+        showLastLabel: true
+      }
+    };
+  };
 
-		return series;
-	}
+  function goToClones(d) {
+    $scope.$state.go("app.clones", {
+      weight: this.x,
+      fragments: this.y
+    });
 
-	var stop = $scope.$watch('cloneData', function(cloneData) {
-		if (cloneData) {
-			stop();
-		  	$scope.showChart = false;
+    return false;
+  }
 
-			series = createSeries(cloneData.clones);
-			var conf = chartConfig();
-			conf.options.colors = generateColors(series.length);
-			conf.series = series;
+  function createSeries(clones) {
+    var mapping = {};
+    clones.forEach(function (clazz) {
+      mapping[clazz.weight] = mapping[clazz.weight] || [];
+      mapping[clazz.weight][clazz.fragments.length] = mapping[clazz.weight][clazz.fragments.length] || [];
+      mapping[clazz.weight][clazz.fragments.length].push(clazz.uid);
+    });
 
-			$scope.chartConfig = conf;
-		  $timeout(function() {
-			$scope.showChart = true;
-		  }, 5);
-		}
-	});
+    var data = {};
+    Object.keys(mapping).forEach(function (w) {
+      Object.keys(mapping[w]).forEach(function (fs) {
+        key = mapping[w][fs].length;
+        point = [parseInt(w), parseInt(fs)];
+        if (data[key] === undefined) {
+          data[key] = {
+            name: key,
+            data: [point]
+          };
+        } else {
+          data[key].data.push(point);
+        }
+      });
+    });
+
+    var sortedKeys = _.sortBy(_.map(_.keys(data), function (key) {
+      return parseInt(key);
+    }), function (key) {
+      return key;
+    });
+
+    var size = sortedKeys.length;
+    var bucketSize = Math.floor(size / BUCKET_COUNT);
+    var extra = size % BUCKET_COUNT;
+    var taken = 0;
+
+    //create separate series for the first bucket.
+
+    var series = [];
+
+    _.take(sortedKeys, bucketSize).forEach(function (key) {
+      series.push(data[key]);
+    });
+    taken += bucketSize;
+
+    while (taken != size) {
+      var take = bucketSize;
+      if (extra > 0) {
+        take += 1;
+        extra -= 1;
+      }
+      var keysToTake = sortedKeys.slice(taken, (taken + take));
+      var serie = {
+        name: _.first(keysToTake) + " - " + _.last(keysToTake),
+        data: []
+      };
+
+      keysToTake.forEach(function (key) {
+        serie.data = serie.data.concat(data[key].data);
+      });
+
+      taken += take;
+      series.push(serie);
+    }
+
+    var w = $scope.$state.params.weight;
+    var fs = $scope.$state.params.fragments;
+    if (w && fs) {
+      selectClassesByDataItem(mapping[w][fs], clones);
+    }
+
+    return series;
+  }
+
+  var stop = $scope.$watch('cloneData', function (cloneData) {
+    if (cloneData) {
+      stop();
+      $scope.showChart = false;
+
+      var series = createSeries(cloneData.clones);
+      var conf = chartConfig();
+      conf.options.colors = generateColors(series.length);
+      conf.series = series;
+
+      $scope.chartConfig = conf;
+      $timeout(function () {
+        $scope.showChart = true;
+      }, 5);
+    }
+  });
 
 });
