7b891ce967c15c2780b5294d65157a81ab5816b1
pacbeckh <pacbeckh@users.noreply.github.com>
2015-12-15 10:49:11 +0100

    Added improved treemap (WIP)

--- server/public/script/controllers/TreeMapCtrl.js
+++ server/public/script/controllers/TreeMapCtrl.js
@@ -1,42 +1,119 @@
 angular.module('CloneDetection').controller('TreeMapCtrl', function ($scope, TreeMapService) {
 
+
     var stop = $scope.$watch('cloneData', function (cloneData) {
         if (cloneData) {
-          console.log(cloneData);
-          var input = prepareData(cloneData)
-
           stop();
-          TreeMapService.render(input);
+
+          console.log(cloneData);
+          var d3Input = prepareData(cloneData)
+
+//          TreeMapService.render(d3input);
+
+//          console.log("start copy");
+          rootFiles = d3Input.children;
+//          console.log("copy fixed");
+          points = createPoints(rootFiles);
+          console.log(points);
+          $scope.treemapConfig = createChartConfig(points);
         }
     });
 
     function prepareData(cloneData){
-      var files = cloneData.files.filter(function (x) {
-        return x.name === "src";
-      });
+       var rootFileIndex = -1;
 
-      var filesCopy = angular.copy(files);
+       var pathQuery = $scope.$state.params.pathQuery;
+       if(pathQuery){
+          rootFileIndex = _.findIndex(cloneData.allFileRefs, 'filePath', pathQuery);
+       }
 
-      result = filesCopy.map(function (x) {
-        return fixChilds(x);
-      });
+       if(rootFileIndex == -1) {
+          rootFileIndex = _.findIndex(cloneData.allFileRefs, function (file){
+            return file.path == "src" || file.path == "hsqldb/src"
+          });
+       }
 
-      console.log(result);
-
-      return result[0];
+      var rootFile = angular.copy(cloneData.allFileRefs[rootFileIndex]);
+      fixChilds(rootFile.children);
+      return rootFile;
     }
 
-    function fixChilds(file){
-      if(file.children.length == 0){
-        file.size = Math.sqrt(file.fragments.length);
-        delete file.children;
-      }else{
-        file.children = file.children.map(function(child){
-           return fixChilds(child);
-        }).filter(function (child){
-          return !child.size || child.size > 0;
+    function fixChilds(children){
+        children.forEach(function(file){
+            if(file.children.length == 0) {
+                file.size = Math.sqrt(file.fragments.length);
+                delete file.children;
+            } else {
+                file.children = fixChilds(file.children);
+            }
         });
-      }
+        return children.filter(function (file){
+          return (file.size === undefined && file.children && file.children.length > 0 ) || file.size > 0;
+        });
+
       return file;
     }
+
+    function createPoints(files, parent){
+      var result = [];
+      files.forEach(function (file){
+         var point = {
+           parent: parent ? parent.path : undefined,
+           name: file.name,
+           id: file.path
+         };
+         result.push(point);
+        if(file.children){
+          result = result.concat(createPoints(file.children, file));
+        } else {
+          point.value = Math.sqrt(file.fragments.length);
+          point.colorValue = Math.sqrt(file.fragments.length);
+        }
+      });
+
+      return result;
+    }
+
+    function createChartConfig(points) {
+      return {
+        options: {
+         colorAxis: {
+                 minColor: 'rgb(0,255,0)',
+                 maxColor: 'rgb(255,0,0)'
+             },
+
+             plotOptions : {
+                treemap: {
+                 tooltip: {
+//                   pointFormat: '{point.value} fragments'
+                 }
+                }
+             }
+         },
+         series: [{
+             type: 'treemap',
+             layoutAlgorithm: 'squarified',
+             allowDrillToNode: true,
+             dataLabels: {
+                 enabled: false
+             },
+             levelIsConstant: false,
+             levels: [{
+                 level: 2,
+                 dataLabels: {
+                     enabled: true
+                 },
+                 borderWidth: 2
+             }],
+             data: points
+         }],
+         subtitle: {
+             text: 'Click points to drill down.'
+         },
+         title: {
+             text: 'Clone distribution'
+         }
+     }
+    }
+
 });
